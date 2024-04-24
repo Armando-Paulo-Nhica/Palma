3871e114a4840823814f57b03a996998b1e94466
Armando Paulo Nhica <armandonhica20@gmail.com>
2024-04-24 17:10:07 +0200

    receipt

--- public/js/components.js
+++ public/js/components.js
@@ -273,29 +273,11 @@ document.getElementById("header").innerHTML =` <div class="header-content">
             <li class="nav-item dropdown notification_dropdown">
                 <a class="nav-link" href="#" role="button" data-toggle="dropdown">
                     <i class="mdi mdi-bell"></i>
-                    <div class="pulse-css"></div>
+                    
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <ul class="list-unstyled">
-                        <li class="media dropdown-item">
-                            <span class="success"><i class="ti-user"></i></span>
-                            <div class="media-body">
-                                <a href="#">
-                                    <p><strong>Martin</strong> has added a <strong>customer</strong> Successfully
-                                    </p>
-                                </a>
-                            </div>
-                            <span class="notify-time">3:20 am</span>
-                        </li>
-                        <li class="media dropdown-item">
-                            <span class="primary"><i class="ti-shopping-cart"></i></span>
-                            <div class="media-body">
-                                <a href="#">
-                                    <p><strong>Jennifer</strong> purchased Light Dashboard 2.0.</p>
-                                </a>
-                            </div>
-                            <span class="notify-time">3:20 am</span>
-                        </li>
+                        
                         <li class="media dropdown-item">
                             <span class="danger"><i class="ti-bookmark"></i></span>
                             <div class="media-body">
@@ -351,7 +333,7 @@ document.getElementById("header").innerHTML =` <div class="header-content">
 
             <li class="nav-item dropdown header-profile">
             <a class="nav-link" href="#" role="button" data-toggle="dropdown">
-                <i class="fas fa-user-gear"></i>
+                <i  class="fas fa-user"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <a href="/user/profile" class="dropdown-item">
